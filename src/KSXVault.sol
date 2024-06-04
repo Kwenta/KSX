8df158b08fad69ecf2aca3f5064e5ee3499022d9
Flocqst <flocastet@icloud.com>
2024-06-04 16:06:03 +0200

    👷 import ERC4626

--- src/KSXVault.sol
+++ src/KSXVault.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
 /// @title Kwenta Example Contract
 /// @notice Responsible for counting
 /// @author Flocqst (florian@kwenta.io)
-contract KSXVault {
+contract KSXVault is ERC4626 {
+  
 }
