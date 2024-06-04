59e2fab29e15774085eebcb4d913d1eb616b5bbc
Flocqst <flocastet@icloud.com>
2024-06-04 16:04:35 +0200

    👷 Initialize KSXVault contract

---
+++ src/KSXVault.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+/// @title Kwenta Example Contract
+/// @notice Responsible for counting
+/// @author Flocqst (florian@kwenta.io)
+contract KSXVault {
+}
