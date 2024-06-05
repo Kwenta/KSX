b0973ba25148dbcfc06644401cb8e27618dd7785
Flocqst <flocastet@icloud.com>
2024-06-05 18:31:05 +0200

    👷 Basic ERC4626 Vault

--- src/KSXVault.sol
+++ src/KSXVault.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import { ERC4626 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import { ERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /// @title Kwenta Example Contract
-/// @notice Responsible for counting
+/// @notice KSX ERC4626 Vault
 /// @author Flocqst (florian@kwenta.io)
 contract KSXVault is ERC4626 {
-  
+    constructor(address _token) ERC4626(IERC20(_token)) ERC20("KSX Vault", "KSX") {
+
+    }
 }
