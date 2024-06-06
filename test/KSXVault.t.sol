e73f438608cb08afa8095acb57babd4f229ebe95
Flocqst <flocastet@icloud.com>
2024-06-06 18:31:43 +0200

    ✨ forge fmt

--- test/KSXVault.t.sol
+++ test/KSXVault.t.sol
@@ -2,19 +2,19 @@
 pragma solidity 0.8.25;
 
 import {Test} from "forge-std/Test.sol";
-import { KSXVault } from "../src/KSXVault.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {KSXVault} from "../src/KSXVault.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract KSXVaultTest is Test {
-    function setUp() public {
-
-    }
+    function setUp() public {}
 }
 
 contract MockERC20 is ERC20 {
-    constructor (string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
+    constructor(string memory name_, string memory symbol_)
+        ERC20(name_, symbol_)
+    {}
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
-    }  
+    }
 }
