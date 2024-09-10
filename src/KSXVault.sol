39444a7cec98110ce831737677d813ae299004b2
Flocqst <flocastet@icloud.com>
2024-09-10 17:02:35 +0200

    👷 Resolve conflicts

--- src/KSXVault.sol
+++ src/KSXVault.sol
@@ -4,8 +4,7 @@ pragma solidity 0.8.25;
 import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC4626} from
     "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import {FixedPointMathLib} from "lib/solady/src/utils/FixedPointMathLib.sol";
-import {IStakingRewardsV2} from "src/interfaces/IStakingRewardsV2.sol";
+import {IStakingRewardsV2} from "@token/interfaces/IStakingRewardsV2.sol";
 
 /// @title KSXVault Contract
 /// @notice KSX ERC4626 Vault
@@ -23,8 +22,12 @@ contract KSXVault is ERC4626 {
     /// (https://docs.openzeppelin.com/contracts/4.x/erc4626#inflation-attack)
     uint8 public immutable offset;
 
-    /// @notice Synthetix v3 perps market proxy contract
+    /// @notice Kwenta's StakingRewards contract
     IStakingRewardsV2 internal immutable STAKING_REWARDS;
+
+    /// @notice KWENTA TOKEN
+     /// @dev The underlying asset of this vault
+    ERC20 private immutable KWENTA;
 
     /*//////////////////////////////////////////////////////////////
                               CONSTRUCTOR
@@ -45,90 +48,89 @@ contract KSXVault is ERC4626 {
     {
         offset = _offset;
         STAKING_REWARDS = IStakingRewardsV2(_stakingRewards);
+        KWENTA = ERC20(_token);
     }
 
+    /// @notice Returns the decimal offset for the vault
+    /// @dev This function is used internally by the ERC4626 implementation
+    /// @return The decimal offset value
     function _decimalsOffset() internal view virtual override returns (uint8) {
         return offset;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        DEPOSIT/MINT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Deposit assets into the vault
+    /// @dev Overrides the ERC4626 deposit function to include reward collection and staking
+    /// @param assets The amount of assets to deposit
+    /// @param receiver The address to receive the minted shares
+    /// @return shares The amount of shares minted
+    function deposit(uint256 assets, address receiver) public virtual override returns (uint256) {
+        uint256 shares = super.deposit(assets, receiver);
+        _collectAndStakeRewards();
+        return shares;
+    }
+
+    /// @notice Mint shares of the vault
+    /// @dev Overrides the ERC4626 mint function to include reward collection and staking
+    /// @param shares The amount of shares to mint
+    /// @param receiver The address to receive the minted shares
+    /// @return assets The amount of assets deposited
+    function mint(uint256 shares, address receiver) public virtual override returns (uint256) {
+        uint256 assets = super.mint(shares, receiver);
+        _collectAndStakeRewards();
+        return assets;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        WITHDRAW/REDEEM FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Withdraw assets from the vault
+    /// @dev Overrides the ERC4626 withdraw function to include unstaking of KWENTA
+    /// @param assets The amount of assets to withdraw
+    /// @param receiver The address to receive the assets
+    /// @param owner The owner of the shares
+    /// @return shares The amount of shares burned
+    function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256) {
+        _unstakeKWENTA(assets);
+        return super.withdraw(assets, receiver, owner);
+    }
+
+    /// @notice Redeem shares of the vault
+    /// @dev Overrides the ERC4626 redeem function to include unstaking of KWENTA
+    /// @param shares The amount of shares to redeem
+    /// @param receiver The address to receive the assets
+    /// @param owner The owner of the shares
+    /// @return assets The amount of assets withdrawn
+    function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256) {
+        uint256 assets = previewRedeem(shares);
+        _unstakeKWENTA(assets);
+        return super.redeem(shares, receiver, owner);
     }
 
     /*//////////////////////////////////////////////////////////////
                         STAKING MANAGEMENT
     //////////////////////////////////////////////////////////////*/
 
-    /// @notice Claim KWENTA rewards
-    function _claimKWENTARewards() internal returns (uint256) {
-        // Implement the logic to claim KWENTA rewards
-    }
+    /// @notice Collect rewards and stake all available KWENTA
+    /// @dev This function is called after every deposit and mint operation
+    function _collectAndStakeRewards() internal {
+        STAKING_REWARDS.getReward();
 
-    /// @notice Stake KWENTA tokens
-    function _stakeKWENTA(uint256 kwentaAmount) internal {
-        STAKING_REWARDS.stake(kwentaAmount);
-    }
-
-    /// @notice Modifier to compound unstaked KWENTA tokens before executing the
-    /// function
-    modifier compoundUnstakedKWENTA() {
-        // StakingV2 harvest rewards
-        // TokenDistributor claim USDC
-        // Swap USDC for KWENTA and stake
-        _;
-    }
-
-
-    /** @dev See {IERC4626-deposit}. */
-    function deposit(uint256 assets, address receiver) public virtual override returns (uint256) {
-        uint256 maxAssets = maxDeposit(receiver);
-        if (assets > maxAssets) {
-            revert ERC4626ExceededMaxDeposit(receiver, assets, maxAssets);
+        uint256 totalToStake = KWENTA.balanceOf(address(this));
+        if (totalToStake > 0) {
+            STAKING_REWARDS.stake(totalToStake);
         }
-
-        uint256 shares = previewDeposit(assets);
-        _deposit(_msgSender(), receiver, assets, shares);
-
-        return shares;
     }
 
-    /** @dev See {IERC4626-mint}.
-     *
-     * As opposed to {deposit}, minting is allowed even if the vault is in a state where the price of a share is zero.
-     * In this case, the shares will be minted without requiring any assets to be deposited.
-     */
-    function mint(uint256 shares, address receiver) public virtual override returns (uint256) {
-        uint256 maxShares = maxMint(receiver);
-        if (shares > maxShares) {
-            revert ERC4626ExceededMaxMint(receiver, shares, maxShares);
-        }
-
-        uint256 assets = previewMint(shares);
-        _deposit(_msgSender(), receiver, assets, shares);
-
-        return assets;
-    }
-
-    /** @dev See {IERC4626-withdraw}. */
-    function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256) {
-        uint256 maxAssets = maxWithdraw(owner);
-        if (assets > maxAssets) {
-            revert ERC4626ExceededMaxWithdraw(owner, assets, maxAssets);
-        }
-
-        uint256 shares = previewWithdraw(assets);
-        _withdraw(_msgSender(), receiver, owner, assets, shares);
-
-        return shares;
-    }
-
-    /** @dev See {IERC4626-redeem}. */
-    function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256) {
-        uint256 maxShares = maxRedeem(owner);
-        if (shares > maxShares) {
-            revert ERC4626ExceededMaxRedeem(owner, shares, maxShares);
-        }
-
-        uint256 assets = previewRedeem(shares);
-        _withdraw(_msgSender(), receiver, owner, assets, shares);
-
-        return assets;
+    /// @notice Unstake KWENTA tokens
+    /// @dev This function is called before withdrawals and redemptions
+    /// @param kwentaAmount The amount of KWENTA to unstake
+    function _unstakeKWENTA(uint256 kwentaAmount) internal {
+        STAKING_REWARDS.unstake(kwentaAmount);
     }
 
 }
